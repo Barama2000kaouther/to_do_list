a8c43bbdefbb00bbbcae0ae2adea39fa195992b6
kaouther <baramakaouther@gmail.com>
2026-05-13 09:03:17 -0700

    fix: link tasks to authenticated user and update CRUD operations

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 # Create your models here.
 class Task(models.Model):
-  #user_id=models.ForeignKey(User,on_delete=models.CASCADE,related_name="tasks")
+  user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="tasks")
   task=models.CharField(max_length=30)
   is_completed=models.BooleanField(default=False)
   
--- tasks/views.py
+++ tasks/views.py
@@ -9,42 +9,45 @@ def get_task_content(task):
   form=TaskForm()
   count_tasks=task.count()
   return {
-       'form':form,
+      'form':form,
       'all_tasks':task,
       'count_tasks':count_tasks
   } 
   
 @login_required
 def index(request):
-  tasks=Task.objects.all().values()
+  tasks=request.user.tasks.all().values()
   context=get_task_content(tasks)
   return render(request,"tasks/index.html",context)
 
 @login_required  
 def filter_task(request,filter_type):
-
+  tasks = request.user.tasks.all()
   if filter_type=='all': 
      return redirect('tasks:index')
     
   elif filter_type=='active':
-      tasks=Task.objects.values().filter(is_completed=False)
+    
+    tasks=tasks.values().filter(is_completed=False)
+    
      
   elif filter_type=='completed':
-      tasks=Task.objects.values().filter(is_completed=True)
+     
+     tasks=tasks.values().filter(is_completed=True)
       
   context=get_task_content(tasks)
   return render(request,"tasks/index.html",context)
       
-  
-  
  
 @login_required
 def add_task(request):
   if request.method=="POST":
     form=TaskForm(request.POST)
     if form.is_valid():
-       form.save()
-       return redirect('tasks:index')
+        task = form.save(commit=False) 
+        task.user = request.user    
+        task.save()       
+        return redirect('tasks:index')
     else:
       return redirect('tasks:index')
     
@@ -53,7 +56,7 @@ def add_task(request):
 @login_required    
 def delete_task(request,task_id):
   if request.method=='POST':
-      deletedTask=get_object_or_404(Task,id=task_id)
+      deletedTask=get_object_or_404(Task,id=task_id,user_id=request.user)
       deletedTask.delete()
       return redirect('tasks:index')
     
@@ -62,7 +65,7 @@ def delete_task(request,task_id):
 @login_required
 def complete_task(request,task_id):
   if request.method=='POST':
-       task = get_object_or_404(Task,id=task_id)
+       task = get_object_or_404(Task,id=task_id,user_id=request.user)
        task.is_completed = not task.is_completed
        task.save()
        return redirect('tasks:index')
@@ -72,7 +75,7 @@ def complete_task(request,task_id):
 @login_required        
 def update_task(request,task_id):
      if request.method=='POST':
-       task = get_object_or_404(Task,id=task_id)
+       task = get_object_or_404(Task,id=task_id,user_id=request.user)
        task.task=request.POST['update_task']
        task.save()
        return redirect('tasks:index')
